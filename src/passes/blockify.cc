50185b84489aea2b98ef6acdf54eb502c630f574
Elias Castegren <elias.castegren@gmail.com>
2025-07-02 19:44:49 +0200

    Dead-code elimination in blockify to handle early returns

--- src/passes/blockify.cc
+++ src/passes/blockify.cc
@@ -3,6 +3,13 @@
 namespace whilelang {
     using namespace trieste;
 
+    std::string get_label(Node node) {
+        if (node != Label) {
+            throw std::runtime_error("Node is not a label");
+        }
+        return std::string(node->location().view());
+    }
+
     PassDef blockify() {
         PassDef blockify = {
             "blockify",
@@ -14,27 +21,53 @@ namespace whilelang {
                      T(ParamList)[ParamList] *
                      (T(Stmt) << T(Block)[Body])) >>
                     [](Match &_) -> Node {
+                        auto fun_body = _(Body);
+                        if (fun_body->size() == 0) {
+                            return Error << (ErrorAst << _(FunId))
+                                         << (ErrorMsg ^ "Empty function body");
+                        } else if (fun_body->at(0) / Stmt != Label) {
+                            return Error << (ErrorAst << _(FunId))
+                                         << (ErrorMsg ^ "Function body must start with a label");
+                        }
+
+                        // Only keep labels that are targeted by jumps
+                        std::set<std::string> targeted_labels;
+                        targeted_labels.insert(get_label(fun_body->at(0) / Stmt));
+
                         Node blocks = Blocks;
                         Node block = Block;
                         Node body = Body;
-                        for (auto child : *(_(Body))) {
+                        for (auto child : *fun_body) {
                             auto stmt = child / Stmt;
-                            if (stmt != Label && block->size() == 0)
-                                return Error << (ErrorAst << _(Body))
-                                             << (ErrorMsg ^ "Block without a label (found " + stmt->str() + ")");
+                            if (stmt != Label && block->size() == 0) {
+                                // Unreachable statement, skip it
+                                continue;
+                            }
 
                             if (stmt == Label) {
+                                if (!targeted_labels.contains(get_label(stmt))) {
+                                    // Label is not targeted, skip it
+                                    continue;
+                                }
                                 if (block->size() == 0) {
                                     // First label in the block
                                     block << stmt;
                                 } else {
                                     // Fallthrough
+                                    targeted_labels.insert(get_label(stmt));
+                                    std::cout << get_label(stmt) << " targeted" << std::endl;
                                     block << body << (Jump << stmt->clone());
                                     blocks << block;
                                     block = Block << stmt;
                                     body = Body;
                                 }
                             } else if (stmt->type().in({Cond, Jump, Return})) {
+                                if (stmt == Jump) {
+                                                                        targeted_labels.insert(get_label(stmt / Label));
+                                } else if (stmt == Cond) {
+                                    targeted_labels.insert(get_label(stmt / Then));
+                                    targeted_labels.insert(get_label(stmt / Else));
+                                }
                                 block << body << stmt;
                                 blocks << block;
                                 block = Block;
@@ -43,8 +76,9 @@ namespace whilelang {
                                 body << child;
                             }
                         }
-                        if (block->size() != 0)
-                            return Error << (ErrorAst << _(Body))
+
+                        if (block->size() > 0)
+                            return Error << (ErrorAst << fun_body)
                                          << (ErrorMsg ^ "Block without a terminator: " + block->str());
 
                         return FunDef << _(FunId) << _(ParamList) << blocks;
